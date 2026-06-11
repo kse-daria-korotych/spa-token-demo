a4ff21819c444fee1b284c12e551570125fa7213
d.korotych <d.korotych@Vladyslavs-MacBook-Pro.local>
2026-06-11 17:18:00 +0200

    4.3 - login using api-security realm

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
   url: "https://keycloak.192.168.151.10.nip.io",
-  realm: "daria-korotych-lab",
-  clientId: "notes-spa",
+  realm: "api-security",
+  clientId: "spa-token-demo",
 });
 
 export default keycloak;
